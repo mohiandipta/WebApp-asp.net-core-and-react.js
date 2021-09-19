fa32ab7bacbef2a59b029c2812d453d2277131dc
mohiandipta <mohianmostofa@gmail.com>
2021-09-19 11:02:05 +0600

    model class ready

---
+++ WebApp asp.net core and react.js/Models/Dept.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp_asp.net_core_and_react.js.Models
+{
+    [Table("Depts")]
+    public class Dept
+    {
+        [Key]
+        [Required]
+        public int DeptNo { get; set; }
+
+        [MaxLength(50)]
+        public string DName { get; set; }
+
+        [MaxLength(50)]
+        public string Location { get; set; }
+    }
+}
